3faea2cc76c86327652d8c175ea70278f67defcb
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-03-20 20:26:17 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,26 +1,16 @@
-import os, time, platform, base64
+import os, platform
 os.system("cd $HOME/")
 #os.system("termux-setup-storage")
 #os.system("xdg-open https://www.facebook.com/100346185630243/posts/248205137511013/?app=fbl")
-try:
-    import dateutil
-except ImportError:
-    os.system("pip install python-dateutil")
-try:
-    import bs4
-except ImportError:
-    os.system("pip install bs4")
+
 try:
     import requests
 except ImportError:
     os.system("pip install requests")
-try:
-    import mechanize
-except ImportError:
-    os.system("pip install mechanize")
+
 
 rana=platform.architecture()[0]
 if rana=="32bit":
-    __import__("rsa32").mysec()
+    __import__("pro32").my_tool_security()
 elif rana=="64bit":
-    __import__("rsa").mysec()
+    __import__("pro").my_tool_security()
