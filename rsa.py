6da7e0e8f8afec1f45052dd7125c40e46fe17d5a
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-04-06 06:09:49 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -2,7 +2,8 @@ import os, platform
 os.system("cd $HOME/")
 #os.system("termux-setup-storage")
 #os.system("xdg-open https://www.facebook.com/100346185630243/posts/248205137511013/?app=fbl")
-
+os.system("clear")
+print("\t Tool updating please wait..")
 try:
     import requests
 except ImportError:
